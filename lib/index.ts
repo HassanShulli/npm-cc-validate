aadd44e8d2bc7a9ac05856860c2537346d04594b
Hassan Shulli <hassan14x@hotmail.com>
2019-07-13 12:18:03 +0800

    local changes hassan

--- lib/index.ts
+++ lib/index.ts
@@ -1,21 +1,21 @@
-
 /**
-* @Method: Returns an object with property isValid equals to True if the credit card number is valid and false if it is not. Object also contains cardType, formatted card number in the form **** **** .. and a helpful message
-* @Param {string}
-* @Return {object}
-*/
+ * @Method: Returns an object with property isValid equals to True if the credit card number is valid and false if it is not. Object also contains cardType, formatted card number in the form **** **** .. and a helpful message
+ * @Param {string}
+ * @Return {object}
+ */
 
-export function isValid (input: string) : object {
+export function isValid(cardNumber: string): object {
     // remove spacing
-    const cardNumber = input.replace(/\s/g,'');
+    cardNumber = cardNumber.replace(/^\s+|\s+$|\s+(?=\s)/g, "");
+
     // initialize response object
     let result = {
-        cardNumber : '',
-        cardType : '',
-        isValid : false,
-        message : ''
-    }
-    
+        cardNumber: '',
+        cardType: 'Unknown',
+        isValid: false,
+        message: ''
+    };
+
     // if input is not a string return false
     if (typeof cardNumber !== "string") {
         result.isValid = false;
@@ -28,7 +28,7 @@ export function isValid (input: string) : object {
             result.isValid = false;
             result.message = 'All zero input is not a valid credit card number';
             return result
-        } 
+        }
     }
 
     result.cardNumber = cardNumber.replace(/(\d{4}(?!\s))/g, '$1 ');
@@ -37,70 +37,78 @@ export function isValid (input: string) : object {
     let toggle = false;
 
     const length = cardNumber.length;
-    
+
     const firstDigit = parseInt(cardNumber.charAt(0));
-    const secondDigit = parseInt(cardNumber.charAt(1));
     const twoDigits = parseInt(cardNumber.substring(0, 2));
+    const threeDigits = parseInt(cardNumber.substring(0, 3));
     const fourDigits = parseInt(cardNumber.substring(0, 4));
     const sixDigits = parseInt(cardNumber.substring(0, 6));
 
-    if ( (twoDigits === 34 ||  twoDigits === 37) && length === 15 ) {
-            result.cardType = 'American Express';
+    if ((twoDigits === 34 || twoDigits === 37) && length === 15) {
+        result.cardType = 'American Express';
     } else if (firstDigit === 4 && (length >= 16 && length <= 19)) {
         result.cardType = 'Visa';
-    } else if ((twoDigits >= 51 && twoDigits <= 55) || 
-                (twoDigits >= 22 && twoDigits <= 27)) {
-                    if (length === 16) {
-                        result.cardType = 'MasterCard';
-                    }
-    } else if ((sixDigits >= 601100 && sixDigits <= 601109)
-            || (sixDigits >= 601120 && sixDigits <= 601149)
-            || (sixDigits >= 601177 && sixDigits <= 601179)
-            || (sixDigits >= 601186 && sixDigits <= 601199)
-            || (sixDigits >= 644000 && sixDigits <= 659999)
-            || sixDigits === 601174
-    ) {
+    } else if ((twoDigits >= 51 && twoDigits <= 55) ||
+        (twoDigits >= 22 && twoDigits <= 27)) {
         if (length === 16) {
+            result.cardType = 'MasterCard';
+        }
+    } else if (sixDigits === 622126 || sixDigits === 622925 ||
+        fourDigits === 6011 || threeDigits === 644 || threeDigits === 645 ||
+        threeDigits === 646 || threeDigits === 647 || threeDigits === 648 ||
+        threeDigits === 649 || twoDigits === 65
+    ) {
+        if (length >= 16 && length <= 19) {
             result.cardType = 'Discover';
         }
     } else if (fourDigits >= 3528 && fourDigits <= 3589) {
         if (length >= 16 && length <= 19) {
             result.cardType = 'JCB';
         }
-    } else if (twoDigits === 50 ||
-            (twoDigits >= 56 && twoDigits <= 64) ||
-            (twoDigits >= 66 && twoDigits <= 69)) {
-                if (length <= 19) {
-                    result.cardType = 'Maestro';
-                }
-    } else if (fourDigits === 2014 || fourDigits === 2149) {
-        if (length === 15) {
-            result.cardType = 'enRoute';
+    } else if (fourDigits === 5018 || fourDigits === 5020 || fourDigits === 5038
+        || fourDigits === 5893 || fourDigits === 6304 || fourDigits === 6759
+        || fourDigits === 6761 || fourDigits === 6762 || fourDigits === 6763) {
+        if (length >= 16 && length <= 19) {
+            result.cardType = 'Maestro';
+        }
+    } else if (threeDigits === 300 || threeDigits === 301 ||
+        threeDigits === 302 || threeDigits === 303 ||
+        threeDigits === 304 || threeDigits === 305) {
+        if (length === 14) {
+            result.cardType = 'Diners Club - Carte Blanche'
+        }
+    } else if (twoDigits === 36) {
+        if (length === 14) {
+            result.cardType = 'Diners Club - International';
+        }
+    } else if (twoDigits === 54) {
+        if (length === 16) {
+            result.cardType = 'Diners Club - USA & Canada'
         }
     }
 
     for (let i = cardNumber.length - 1; i >= 0; i--) {
-      const currentNumber = parseInt(cardNumber.charAt(i), 10);
-      if (toggle === true) {
-        if (currentNumber * 2 > 9) {
-          const digitOne = (currentNumber * 2) % 10;
-          const digitTwo = Math.floor((currentNumber * 2) / 10);
-          sum += digitOne + digitTwo;
+        const currentNumber = parseInt(cardNumber.charAt(i), 10);
+        if (toggle === true) {
+            if (currentNumber * 2 > 9) {
+                const digitOne = (currentNumber * 2) % 10;
+                const digitTwo = Math.floor((currentNumber * 2) / 10);
+                sum += digitOne + digitTwo;
+            } else {
+                sum += currentNumber * 2;
+            }
         } else {
-          sum += currentNumber * 2;
+            sum += currentNumber;
         }
-      } else {
-        sum += currentNumber;
-      }
-      toggle = !toggle;
+        toggle = !toggle;
     }
     if (length < 13 || length > 19) {
         result.isValid = false;
         result.message = 'Credit Card number entered is not valid';
         return result
     } else {
-      result.isValid =  (sum % 10 === 0);
-      result.message = 'Credit Card number entered is valid';
-      return result
+        result.isValid = (sum % 10 === 0);
+        result.message = 'Credit Card number entered valid';
+        return result
     }
-  }
+}
